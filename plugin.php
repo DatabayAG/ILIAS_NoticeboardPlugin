d104761f76e4647f0f3b953a60f30f3b36684195
nmatuschek <nmatuschek@databay.de>
2017-11-14 10:56:10 +0100

    Plugin Version for 5.3.x

--- plugin.php
+++ plugin.php
@@ -4,13 +4,13 @@
 $id = 'xnob';
 
 // code version; must be changed for all code changes
-define('xnob_version', '1.2.0');
+define('xnob_version', '2.0.0');
 $version = xnob_version;
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = '5.0.0';
-$ilias_max_version = '5.2.999';
+$ilias_min_version = '5.3.0';
+$ilias_max_version = '5.3.999';
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = 'Jens Conze / Nadia Matuschek';
